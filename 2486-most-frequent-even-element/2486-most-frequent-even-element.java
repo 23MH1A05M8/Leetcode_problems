c3ba8e886728dfa3220465c7dccc754f10da6f74
KUNISETTY HADASSA <155531053+23MH1A05M8@users.noreply.github.com>
2025-09-18 23:07:06 +0530

    Time: 37 ms (12.75%), Space: 45.2 MB (46.69%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.java
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.java
@@ -7,18 +7,20 @@ class Solution {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
             }
         }
-        int max=0;
-        for(int i:map.values())
-        {
-            max=Math.max(max,i);
-        }
+       
+        if(map.isEmpty()) 
+       {
+         return -1;
+       }
+        int max=0,res=-1;
         for(Map.Entry<Integer,Integer> i:map.entrySet())
         {
-            if(i.getValue()==max)
+            if(i.getValue()>max)
             {
-                return i.getKey();
+                max=i.getValue();
+                res=i.getKey();
             }
         }
-        return -1;
+        return res;
     }
 }
