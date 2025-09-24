70952d6e29503ef9e68c562c09471277bcd92fde
KUNISETTY HADASSA <155531053+23MH1A05M8@users.noreply.github.com>
2025-09-24 18:11:03 +0530

    Time: 5 ms (41.7%), Space: 42.5 MB (39.97%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+      Stack<Character> st=new Stack<>();
+      StringBuilder res=new StringBuilder();
+      for(char c:s.toCharArray())
+      {
+            if(c=='(')
+            {
+                if(!st.isEmpty())
+                {
+                    res.append(c);
+                }
+                st.push(c);
+            }
+            else {
+                st.pop();
+                if(!st.isEmpty())
+                {
+                    res.append(c);
+                }
+            }
+            
+      }  
+      return res.toString();
+    }
+}
