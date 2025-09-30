1b03ac47509272c31bd2c9423ae15c5e3a07589c
KUNISETTY HADASSA <155531053+23MH1A05M8@users.noreply.github.com>
2025-09-30 16:02:08 +0530

    Time: 0 ms (100%), Space: 40.6 MB (28.75%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,21 @@
 class Solution {
     public int fib(int n) {
-        if(n==0||n==1)
+        // if(n==0||n==1)
+        // {
+        //     return n;
+        // }
+        // return fib(n-1)+fib(n-2);
+        if(n==0)
+        return 0;
+        if(n==1)
+        return 1;
+        int[] fibarr=new int[n+1];
+        fibarr[0]=0;
+        fibarr[1]=1;
+        for(int i=2;i<=n;i++)
         {
-            return n;
+            fibarr[i]=fibarr[i-1]+fibarr[i-2];
         }
-        return fib(n-1)+fib(n-2);
+        return fibarr[n];
     }
 }
