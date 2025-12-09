211593f2a790946c4a783e9592f621dbe5ac193a
KUNISETTY HADASSA <155531053+23MH1A05M8@users.noreply.github.com>
2025-12-09 14:55:32 +0530

    Time: 0 ms (100%), Space: 12.5 MB (8.7%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+         vector<int> v;
+        for(int i=0;i<m;i++)
+        {
+                v.push_back(nums1[i]);
+            
+        }
+        for(int i=0;i<nums2.size();i++)
+        {
+           
+            
+                v.push_back(nums2[i]);
+        }
+        
+        
+        sort(v.begin(),v.end());
+        for(int i=0;i<v.size();i++)
+        {
+            
+            nums1[i] = v[i];
+        }
+    }
+};
