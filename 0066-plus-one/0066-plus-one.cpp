bffb8b45f4e1c278407d39d9dcede16df8bc6cf1
KUNISETTY HADASSA <155531053+23MH1A05M8@users.noreply.github.com>
2025-12-13 21:30:03 +0530

    Time: 3 ms (5.52%), Space: 11.6 MB (14.56%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        // vector<int> v;
+       for(int i=digits.size()-1;i>=0;i--)
+       {
+         while(digits[i]<9)
+         {
+            digits[i]++;
+            return digits;
+         }
+         digits[i]=0;
+       }
+       digits.insert(digits.begin(),1);
+       return digits;
+    }
+};
