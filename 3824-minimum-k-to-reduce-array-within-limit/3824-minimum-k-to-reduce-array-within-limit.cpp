61659fb17936757a4d09ce57186382a0e0d18bfb
KUNISETTY HADASSA <23MH1A05M8@acoe.edu.in>
2026-02-21 15:43:50 +0530

    Time: 149 ms (12.62%), Space: 197.4 MB (8.89%) - LeetHub

---
+++ 3824-minimum-k-to-reduce-array-within-limit/3824-minimum-k-to-reduce-array-within-limit.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumK(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        long long l=0,h=1e5+1;
+        while(l+1<h)
+            {
+                long long k=(l+h)/2;
+                int c=0;
+                for(int i=0;i<n;i++)
+                {
+                    if(nums[i]%k==0)
+                    {
+                        c=c+nums[i]/k;
+                    }
+                    else{
+                        c=c+(nums[i]/k)+1;
+                    }
+                }
+                if(c<=1LL*k*k)
+                {
+                    h=k;
+                }
+                else{
+                    l=k;
+                }
+            }
+        return l+1;
+    }
+};
