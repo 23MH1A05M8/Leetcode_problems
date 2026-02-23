32cd248c4808cad51d9780a5bc35ded69a03920f
KUNISETTY HADASSA <23MH1A05M8@acoe.edu.in>
2026-02-23 14:28:36 +0530

    Time: 26 ms (25.77%), Space: 7.9 MB (90.41%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -5,7 +5,7 @@ public:
         if(n<=1) return false;
         if(n==2) return true;
         if(n%2==0) return false;
-        for(int i=3;i*i<=n;i++)
+        for(int i=3;i*i<=n;i+=2)
         {
             if(n%i==0)
             {
