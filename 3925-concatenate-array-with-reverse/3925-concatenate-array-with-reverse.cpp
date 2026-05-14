63a295406c8bcd940289670f56bf9922980057dc
KUNISETTY HADASSA <23MH1A05M8@acoe.edu.in>
2026-05-14 21:51:04 +0530

    Time: 0 ms (100%), Space: 30.3 MB (64.78%) - LeetHub

---
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> concatWithReverse(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> v(2*n,0);
+        for(int i=0;i<n;i++)
+        {
+            v[i]=nums[i];
+            v[i+n]=nums[n-i-1];
+        }
+        return v;
+    }
+};
