355ce99d05fe95722c1814b0fe77324c8f7b8b5a
KUNISETTY HADASSA <23MH1A05M8@acoe.edu.in>
2026-06-16 21:48:11 +0530

    Time: 2 ms (79.92%), Space: 39.8 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string processStr(string s) {
+        int n=s.size();
+        string res="";
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]>='a'&&s[i]<='z')
+            {
+                res+=s[i];
+            }
+            else if(s[i]=='*'&&res.size()!=0)
+            {
+                res.pop_back();
+            }
+            else if(s[i]=='#')
+            {
+                res+=res;
+            }
+            else{
+                reverse(res.begin(),res.end());
+            }
+        }
+        return res;
+    }
+};
