f8c1ec38c3a42b12aa01785afb56ada3c17c721c
KUNISETTY HADASSA <23MH1A05M8@acoe.edu.in>
2026-06-16 23:48:52 +0530

    Time: 0 ms (100%), Space: 14.9 MB (86.16%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if(root==NULL) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int s=q.size();
+            for(int i=0;i<s;i++)
+            {
+                TreeNode* top_ele=q.front();
+                q.pop();
+                if(i==s-1)
+                {
+                    res.push_back(top_ele->val);
+                }
+                if(top_ele->left!=NULL)
+                    q.push(top_ele->left);
+                if(top_ele->right!=NULL)
+                    q.push(top_ele->right);
+            }
+        }
+        return res;
+    }
+};
