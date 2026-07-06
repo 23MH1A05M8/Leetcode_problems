14d2446c3e384795c39cbe3931922998a078196f
KUNISETTY HADASSA <23MH1A05M8@acoe.edu.in>
2026-07-06 11:11:16 +0530

    Time: 0 ms (100%), Space: 15.2 MB (30.6%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static bool cmp(vector<int> &a,vector<int> &b)
+    {
+        if(a[0]==b[0]) return a[1]>b[1];
+        return a[0]<b[0];
+    }
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+      int n=intervals.size();
+      sort(intervals.begin(),intervals.end(),cmp);
+      int st=intervals[0][0];
+      int end=intervals[0][1];
+      vector<pair<int,int>> res;
+      res.push_back({st,end});
+      for(int i=1;i<n;i++)
+      {
+        if((intervals[i][0]>=st&&intervals[i][1]<=end))
+        {
+            end=max(end,intervals[i][1]);
+        }
+        else{
+            res.push_back({st,end});
+            st=intervals[i][0];
+            end=intervals[i][1];
+        }
+      }  
+      return res.size();
+    }
+};
