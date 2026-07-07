9a9d4eef748c964cadfa0e09cbf11a24cc03efe3
KUNISETTY HADASSA <23MH1A05M8@acoe.edu.in>
2026-07-07 21:58:16 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
+        if(n<=0) return false;
         if(n==1) return true;
-        if(n%2==0){
-           return isPowerOfTwo(n/2);
-        }
+        // if(n%2==0){
+        //    return isPowerOfTwo(n/2);
+        // }
+        // return false;
+        if((n&(n-1))==0) return true;
         return false;
     }
 };
